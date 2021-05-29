f2098e95a3598e5002219c753fda31e8b9cb9ab8
ryunosuke35 <6bev2103@gmail.com>
2021-05-29 16:55:18 +0900

    デザインの変更とヘッダーメニューリンクの設定

---
+++ app/models/feed.rb
@@ -0,0 +1,5 @@
+class Feed < ApplicationRecord
+  mount_uploader :image, ImageUploader
+  validates :image, presence: true
+  belongs_to :user
+end
---
+++ db/schema.rb
@@ -0,0 +1,23 @@
+ActiveRecord::Schema.define(version: 2021_05_28_022205) do
+  enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_feeds_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "feeds", "users"
+end
